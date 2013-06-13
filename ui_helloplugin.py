27c771e613291d3ea9be34f75d2056a669e06b54
Ismail Sunni <imajimatika@gmail.com>
2013-06-13 17:12:34 +0700

    compile edited UI.

--- ui_helloplugin.py
+++ ui_helloplugin.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui_helloplugin.ui'
 #
-# Created: Thu Jun 13 16:13:50 2013
+# Created: Thu Jun 13 17:23:18 2013
 #      by: PyQt4 UI code generator 4.10
 #
 # WARNING! All changes made in this file will be lost!
@@ -26,18 +26,36 @@ except AttributeError:
 class Ui_HelloPlugin(object):
     def setupUi(self, HelloPlugin):
         HelloPlugin.setObjectName(_fromUtf8("HelloPlugin"))
-        HelloPlugin.resize(400, 300)
-        self.buttonBox = QtGui.QDialogButtonBox(HelloPlugin)
-        self.buttonBox.setGeometry(QtCore.QRect(30, 240, 341, 32))
-        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
-        self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
-        self.buttonBox.setObjectName(_fromUtf8("buttonBox"))
+        HelloPlugin.resize(400, 266)
+        HelloPlugin.setAcceptDrops(False)
+        self.formLayout = QtGui.QFormLayout(HelloPlugin)
+        self.formLayout.setObjectName(_fromUtf8("formLayout"))
+        self.enterNameLabel = QtGui.QLabel(HelloPlugin)
+        font = QtGui.QFont()
+        font.setFamily(_fromUtf8("Tahoma"))
+        font.setBold(True)
+        font.setWeight(75)
+        self.enterNameLabel.setFont(font)
+        self.enterNameLabel.setObjectName(_fromUtf8("enterNameLabel"))
+        self.formLayout.setWidget(0, QtGui.QFormLayout.LabelRole, self.enterNameLabel)
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.nameLineEdit = QtGui.QLineEdit(HelloPlugin)
+        self.nameLineEdit.setObjectName(_fromUtf8("nameLineEdit"))
+        self.horizontalLayout.addWidget(self.nameLineEdit)
+        self.OKPushButton = QtGui.QPushButton(HelloPlugin)
+        self.OKPushButton.setObjectName(_fromUtf8("OKPushButton"))
+        self.horizontalLayout.addWidget(self.OKPushButton)
+        self.formLayout.setLayout(1, QtGui.QFormLayout.SpanningRole, self.horizontalLayout)
+        self.outputPlainTextEdit = QtGui.QPlainTextEdit(HelloPlugin)
+        self.outputPlainTextEdit.setObjectName(_fromUtf8("outputPlainTextEdit"))
+        self.formLayout.setWidget(2, QtGui.QFormLayout.SpanningRole, self.outputPlainTextEdit)
 
         self.retranslateUi(HelloPlugin)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("accepted()")), HelloPlugin.accept)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("rejected()")), HelloPlugin.reject)
         QtCore.QMetaObject.connectSlotsByName(HelloPlugin)
 
     def retranslateUi(self, HelloPlugin):
-        HelloPlugin.setWindowTitle(_translate("HelloPlugin", "HelloPlugin", None))
+        HelloPlugin.setWindowTitle(_translate("HelloPlugin", "Hello Plugin", None))
+        self.enterNameLabel.setText(_translate("HelloPlugin", "Enter you name:", None))
+        self.OKPushButton.setText(_translate("HelloPlugin", "OK", None))
 
